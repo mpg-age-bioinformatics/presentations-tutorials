5c3ff61f2ed8f965c6627770dadff28b33638e1e
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2017-12-12 13:34:09 +0100

    tuxedo.script.sh

--- _site/presentations/modules/rnaseq-tuxedo-update/tuxedo.script.sh
+++ _site/presentations/modules/rnaseq-tuxedo-update/tuxedo.script.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-SHIFTER="shifter --image=mpgagebioinformatics/bioinformatics_software:v1.1.0"
+mkdir -p ~/tuxedo-update/slurm_logs
+LOGS=$(readlink -f ~/tuxedo-update/slurm_logs)/
+
+module load shifter
+SHIFTER="shifter --image=mpgagebioinformatics/bioinformatics_software:v1.1.1"
 
 # ************************************************************************
 # download raw data
-mkdir -p
+mkdir -p ~/tuxedo-update/raw_data
 cd ~/tuxedo-update/raw_data
 
 wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE32nnn/GSE32038/suppl/GSE32038%5Fsimulated%5Ffastq%5Ffiles%2Etar%2Egz
@@ -20,8 +24,8 @@ cd ~/tuxedo-update/references
 wget ftp://ftp.ensembl.org/pub/release-90/fasta/drosophila_melanogaster/dna//Drosophila_melanogaster.BDGP6.dna.toplevel.fa.gz
 wget ftp://ftp.ensembl.org/pub/release-90/gtf/drosophila_melanogaster//Drosophila_melanogaster.BDGP6.90.gtf.gz
 
-tar -zxvf Drosophila_melanogaster.BDGP6.dna.toplevel.fa.gz
-tar -zxvf Drosophila_melanogaster.BDGP6.90.gtf.gz
+unpigz Drosophila_melanogaster.BDGP6.dna.toplevel.fa.gz
+unpigz Drosophila_melanogaster.BDGP6.90.gtf.gz
 
 # ************************************************************************
 
@@ -59,16 +63,15 @@ ln -s GSM794488_C2_R3_2.fq.gz S_006-F_TuUp-L____C2-___-____-REP_3-READ_2.fastq.g
 # ************************************************************************
 
 # Quality control raw data
-mkdir ~/tuxedo-update/fastqc_output
-cd ~/tuxedo-update/raw_data
 
-mkdir -p ~/tuxedo-update/slurm_logs
+mkdir -p ~/tuxedo-update/fastqc_output
+cd ~/tuxedo-update/raw_data
 
 for file in $(ls S_*.fastq.gz);
   do sbatch << EOF
 #!/bin/bash
-#SBATCH -p himem,hugememe,blade,dontuseme
-#SBATCH -o ~/tuxedo-update/slurm_logs/${file%.fastq.gz}.fastqc.out
+#SBATCH -p himem,hugemem,blade,dontuseme
+#SBATCH -o ${LOGS}${file%.fastq.gz}.fastqc.out
 #SBATCH -c 2
 
 ${SHIFTER} << SHI
@@ -82,12 +85,13 @@ SHI
 EOF
 done
 
+
 # ************************************************************************
 
 # Mapping & Assembly & Quantification
 
-mkdir ~/tuxedo-update/hisat2_output
-mkdir ~/tuxedo-update/stringtie_output
+mkdir -p ~/tuxedo-update/hisat2_output
+mkdir -p ~/tuxedo-update/stringtie_output
 
 cd ~/tuxedo-update/raw_data
 
@@ -95,10 +99,10 @@ R=$(readlink -f ../references/Drosophila_melanogaster.BDGP6.dna.toplevel.fa)
 
 IDS=""
 for file in $(ls S_*READ_1.fastq.gz);
-  do IDS=:${IDS}$(sbatch --parsable << EOF
+  do ID=$(sbatch --parsable << EOF
 #!/bin/bash
-#SBATCH -p himem,hugememe,blade,dontuseme
-#SBATCH -o ~/tuxedo-update/slurm_logs/${file%-READ_1.fastq.gz}.hisat.stringtie.out
+#SBATCH -p himem,hugemem,blade,dontuseme
+#SBATCH -o ${LOGS}${file%-READ_1.fastq.gz}.hisat.stringtie.out
 #SBATCH -c 8
 
 ${SHIFTER} << SHI
@@ -110,46 +114,55 @@ module load hisat
 
 hisat2 -p 8 --dta-cufflinks \
 --met-file ../hisat2_output/${file%-READ_1.fastq.gz}.stats \
--x ${R} -1 ${file} -2 ${file%1.fastq.gz}2.fastq.gz
+-x ${R} -1 ${file} -2 ${file%1.fastq.gz}2.fastq.gz \
+-S ../hisat2_output/${file%-READ_1.fastq.gz}.sam
 
 # compress and sort alignments
 module load samtools
 
 cd ~/tuxedo-update/hisat2_output
-samtools view -@ 8 -bhS -F 4 ${file%-READ_1.fastq.gz}.sam | samtools sort -@ 8 -o ${file-READ_1.fastq.gz}.bam -
+samtools view -@ 8 -bhS -F 4 ${file%-READ_1.fastq.gz}.sam | samtools sort -@ 8 -o ${file%-READ_1.fastq.gz}.bam -
 
 # assemly and quantification with stringtie
 module load stringtie
 
-stringtie ${file-READ_1.fastq.gz}.bam \
+stringtie ${file%-READ_1.fastq.gz}.bam \
 -o ../stringtie_output/${file%-READ_1.fastq.gz}.gtf \
 -p 18 -f 0.99 \
--G ../stringtie_output/${file%-READ_1.fastq.gz}.gtf \
+-G ../references/Drosophila_melanogaster.BDGP6.90.gtf \
 -C ../stringtie_output/${file%-READ_1.fastq.gz}_full_cov.gtf \
 -b ../stringtie_output/${file%-READ_1.fastq.gz}
 
 SHI
 EOF
 )
+
+IDS=${IDS}:${ID}
 done
 
 # ************************************************************************
 
-# wait for hisat and stringtie jobs to finish
+# wait for jobs to finish
 
-echo "Waiting for jobs to finish"
+echo "Waiting for jobs to finish" ${IDS}
 srun -p himem,hugemem,blade,dontuseme -d afterok${IDS} echo "jobs completed"
 
 # ************************************************************************
 
 # Merging assemblies
 
-mkdir ~/tuxedo-update/cuffmerge_output
+mkdir -p ~/tuxedo-update/cuffmerge_output
 cd ~/tuxedo-update/stringtie_output
 
-for file in $(ls *.gtf);
+rm -rf assemblies.txt
+for file in $(ls *.gtf | grep -v _full_cov);
   do readlink -f ${file} >> assemblies.txt
 done
+
+# bug: S_002-F_TuUp-L____C1-___-____-REP_2.gtf shows 2x the transcript FBtr0346424
+
+cat S_002-F_TuUp-L____C1-___-____-REP_2.gtf | grep -v FBtr0346424 > S_002-F_TuUp-L____C1-___-____-REP_2.gtf_
+mv S_002-F_TuUp-L____C1-___-____-REP_2.gtf_ S_002-F_TuUp-L____C1-___-____-REP_2.gtf
 
 ${SHIFTER} << SHI
 #!/bin/bash
@@ -165,15 +178,15 @@ SHI
 
 # Post assembly merge and quantification
 
-mkdir ~/tuxedo-update/cuffquant_output
+mkdir -p ~/tuxedo-update/cuffquant_output
 cd ~/tuxedo-update/hisat2_output
 
 IDS=""
 for file in $(ls *.bam);
-  do IDS=:${IDS}$(sbatch --parsable << EOF
+  do ID=$(sbatch --parsable << EOF
 #!/bin/bash
-#SBATCH -p himem,hugememe,blade,dontuseme
-#SBATCH -o ~/tuxedo-update/slurm_logs/${file%-READ_1.fastq.gz}.cuffquant.out
+#SBATCH -p himem,hugemem,blade,dontuseme
+#SBATCH -o ${LOGS}${file%.bam}.cuffquant.out
 #SBATCH -c 8
 
 ${SHIFTER} << SHI
@@ -188,11 +201,14 @@ cuffquant -p 8 --library-type fr-unstranded -o ../cuffquant_output/${file%.bam} 
 SHI
 EOF
 )
+
+IDS=${IDS}:${ID}
+
 done
 
 # ************************************************************************
 
-# wait for hisat and stringtie jobs to finish
+# wait for jobs to finish
 
 echo "Waiting for jobs to finish"
 srun -p himem,hugemem,blade,dontuseme -d afterok${IDS} echo "jobs completed"
@@ -201,13 +217,13 @@ srun -p himem,hugemem,blade,dontuseme -d afterok${IDS} echo "jobs completed"
 
 # Differential analysis
 
-mkdir ~/tuxedo-update/cuffdiff_output
+mkdir -p ~/tuxedo-update/cuffdiff_output
 cd ~/tuxedo-update/cuffquant_output
 
-IDS=:$(sbatch --parsable << EOF
+IDS=$(sbatch --parsable << EOF
 #!/bin/bash
-#SBATCH -p himem,hugememe,blade,dontuseme
-#SBATCH -o ~/tuxedo-update/slurm_logs/cuffdiff.out
+#SBATCH -p himem,hugemem,blade,dontuseme
+#SBATCH -o ${LOGS}cuffdiff.out
 #SBATCH -c 8
 
 ${SHIFTER} << SHI
@@ -229,54 +245,59 @@ EOF
 
 # ************************************************************************
 
-# wait for hisat and stringtie jobs to finish
+# wait for jobs to finish
 
-echo "Waiting for jobs to finish"
-srun -p himem,hugemem,blade,dontuseme -d afterok${IDS} echo "jobs completed"
+echo "Waiting for jobs to finish:"${IDS}
+srun -p himem,hugemem,blade,dontuseme -d afterok:${IDS} echo "jobs completed"
 
 # ************************************************************************
 
 # QC and transcriptome wide plots
 
-mkdir ~/tuxedo-update/cummeRbund_output
+mkdir -p ~/tuxedo-update/cummeRbund_output
 cd ~/tuxedo-update/
+
+INPUT=$(readlink -f cuffdiff_output)
+OUTPUT=$(readlink -f cummeRbund_output)
 
 wget https://raw.githubusercontent.com/mpg-age-bioinformatics/htseq-tools/master/QC.R
 chmod +x QC.R
 
 sbatch << EOF
 #!/bin/bash
-#SBATCH -p himem,hugememe,blade,dontuseme
-#SBATCH -o ~/tuxedo-update/slurm_logs/cummeRbund.out
+#SBATCH -p himem,hugemem,blade,dontuseme
+#SBATCH -o ${LOGS}cummeRbund.out
 
 ${SHIFTER} << SHI
 #!/bin/bash
 source ~/.bashrc
 
 module load rlang
-./QC.R cuffdiff_output cuffquant_output
+rm -rf ${INPUT}cuffData.db
+./QC.R ${INPUT} ${OUTPUT}
 
 SHI
 EOF
-
 
 # ************************************************************************
 
 # Annotation, enrichment and network building
 
-mkdir ~/tuxedo-update/adiff_output
+mkdir -p ~/tuxedo-update/adiff_output
 cd ~/tuxedo-update
 
 sbatch << EOF
 #!/bin/bash
-#SBATCH -p himem,hugememe,blade,dontuseme
-#SBATCH -o ~/tuxedo-update/slurm_logs/adiff.out
+#SBATCH -p himem,hugemem,blade,dontuseme
+#SBATCH -o ${LOGS}adiff.out
 
 ${SHIFTER} << SHI
 #!/bin/bash
 source ~/.bashrc
 
 module load python
+
+export PATH=~/.software_container/.python/2.7.13/bin:$PATH
 
 aDiff -D -i cuffdiff_output -o adiff_output \
 -G references/Drosophila_melanogaster.BDGP6.90.gtf \
@@ -288,8 +309,9 @@ aDiff -D -i cuffdiff_output -o adiff_output \
 --DAVIDid FLYBASE_GENE_ID \
 --DAVIDuser jorge.boucas@age.mpg.de \
 --organismtag DMEL \
---species 'drosophila melanogaster' \
---cytoscape_host '192.168.50.110'
+--species='drosophila melanogaster' \
+--cytoscape_host=localhost \
+--cytoscape_port=1234
 
 SHI
 EOF
